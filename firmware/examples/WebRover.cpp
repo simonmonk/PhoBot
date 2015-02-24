7c626813d3724bbc42ee076e263769cc82a9c858
Simon Monk <srmonk@gmail.com>
2015-02-24 14:03:47 +0000

    Update WebRover.cpp

--- firmware/examples/WebRover.cpp
+++ firmware/examples/WebRover.cpp
@@ -21,14 +21,6 @@ double distance = 0.0;
 PhotonBot p = PhotonBot();
 HC_SR04 rangefinder = HC_SR04(p.trigPin, p.echoPin);
 
-int control(String command) {
-    return p.control(command);
-}
-
-int setMotors(String command) {
-    return p.setMotors(command);
-}
-
 void setup() {
     Spark.function("control", control);
     Spark.function("setmotors", setMotors);
@@ -39,4 +31,12 @@ void setup() {
 void loop() {
     volts = p.batteryVolts();
     distance = rangefinder.getDistanceCM();
+}
+
+int control(String command) {
+    return p.control(command);
+}
+
+int setMotors(String command) {
+    return p.setMotors(command);
 }
